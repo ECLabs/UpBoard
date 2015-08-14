8d1545fc8a7f42e9a54dc68e7b4e375e8b219d4c
niachristian <nia.christian@evanschambers.com>
2015-08-13 22:07:16 -0400

    Create index.php
    This is the major page scene which will show the slides as well as their label. On this page you are able to upload and delete a slide.

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PHP File Uploader</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="boostrap/css/bootstrap.min.css" rel="stylesheet">
+   
+  </head>
+
+  <body>
+
+    <!-- Static navbar -->
+    <div class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">PHP File Uploader</a>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
+    
+    	<div class="row">
+	       <?php 
+	       	//scan "uploads" folder and display them accordingly
+	       $folder = "uploads";
+	       $results = scandir('uploads');
+	       foreach ($results as $result) {
+	       	if ($result === '.' or $result === '..') continue;
+	       
+	       	if (is_file($folder . '/' . $result)) {
+	       		echo '
+	       		<div class="col-md-3">
+		       		<div class="thumbnail">
+			       		<img src="'.$folder . '/' . $result.'" alt="...">
+				       		<div class="caption">
+				       		<p><a href="remove.php?name='.$result.'" class="btn btn-danger btn-xs" role="button">Remove</a></p>
+			       		</div>
+		       		</div>
+	       		</div>';
+	       	}
+	       }
+	       ?>
+    	</div>
+    	
+		
+
+	      <div class="row">
+	      	<div class="col-lg-12">
+	           <form class="well" action="upload.php" method="post" enctype="multipart/form-data">
+				  <div class="form-group">
+				    <label for="file">Select a file to upload</label>
+				    <input type="file" name="file">
+				    <p class="help-block">Only jpg,jpeg,png and gif file with maximum size of 1 MB is allowed.</p>
+				  </div>
+				  <input type="submit" class="btn btn-lg btn-primary" value="Upload">
+				</form>
+			</div>
+	      </div>
+    </div> <!-- /container -->
+<script src="boostrap/js/dropzone.js"></script>
+  </body>
+</html>
