44552c4dfeed190b6607b276269ef9dee6360462
ryangrossGitHub <ryan.gross.01@gmail.com>
2015-10-08 19:32:10 -0400

    Added full screen picture with caption slide.

---
+++ app/scripts/picture.js
@@ -0,0 +1,33 @@
+var picture = function(data, order, previous){
+    var current = (previous+1); //find out what the current slide is in the array
+    var copyArray = order.slice(); //make a copy of the array
+    copyArray.splice(0,1); //remove the current slide from the copy of the order array so we can call copyArray[0] to call the next function easily later
+
+    //insert caption
+    $('.picture p').html(data[current].content.caption);
+
+    //first remove hidden class from slide elements
+    $('.picture').removeClass('hidden');
+
+    if (data[current].transitions.entry === 'fade') {
+        //fade container back in
+        $(".container").fadeIn(295);
+    }
+
+    setTimeout(function() { //Fade Container
+
+            $(".container").fadeOut("slow"); //first fade out the current container
+            //wait & call the rest of the transition
+
+            setTimeout(function() { //transition timing
+
+                    $('.picture').addClass('hidden');
+                    if (copyArray.length!==0) {
+                        window[copyArray[0]](data, copyArray, previous+1);
+                    }
+
+            }, data[current].timing.transitionTime);
+
+    }, data[current].timing.slideTime);
+
+}
