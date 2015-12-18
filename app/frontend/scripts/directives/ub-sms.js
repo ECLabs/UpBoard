0b2051a6d71a8036fcecc17324064d0f45a51472
pauly815 <pauly815@hotmail.com>
2015-12-18 08:52:04 -0500

    UP-23 - updated to display multiple messages

--- app/frontend/scripts/directives/ub-sms.js
+++ app/frontend/scripts/directives/ub-sms.js
@@ -22,6 +22,8 @@
           link: function(scope, element, attrs){
 
             var savedData = null; // use to remember for exit transition
+            
+            scope.messages = [];
 
             scope.$watch(attrs.ngShow, function(){
 
@@ -31,23 +33,26 @@
                 $log.debug('about to show ' + scope.data.type);
 
                 //reset data first
-                element.find('#smsMessage')[0].innerHTML = '';
-                element.find('#smsTimestamp')[0].innerHTML = '';
+                element.find('#defaultMessage')[0].innerHTML = '';
 
-                // if message is expired, set default message instead
+                element.find('#defaultMessage')[0].innerHTML = scope.data.content.default;
+                
+                // if last message is expired, clear out messages
                 var now = new Date().getTime();
                 var messageExpireTime = scope.data.content.timestamp + scope.data.content.timeout;
-                if(messageExpireTime > now) {
-                  element.find('#smsMessage')[0].innerHTML = scope.data.content.message;
-                  element.find('#smsTimestamp')[0].innerHTML = $filter('date')(new Date(scope.data.content.timestamp), 'shortTime');
+                if(messageExpireTime < now){
+                  scope.messages = [];
                 }
-                else element.find('#smsMessage')[0].innerHTML = scope.data.content.default;
 
                 // bind sms slide timestamp to directive scope
                 var timestampPath = 'users/' + Auth.$getAuth().uid + '/decks/' + scope.data.activeDeckId + 
                                     '/slides/' + scope.data.slideId + '/content/timestamp';
                 
                 scope.timestamp = $firebaseObject(Ref.child(timestampPath));
+                
+                // save active deck id and slide id for future bindings
+                scope.activeDeckId = scope.data.activeDeckId;
+                scope.slideId = scope.data.slideId;
                 
                 utility.setEntryTransition(element, scope.data);
                 savedData = scope.data;
@@ -65,6 +70,16 @@
               if((Number(attrs.timestamp) + 1000) > new Date().getTime()){
                 $log.debug('message received!');
                 toaster.pop('success', '', 'Message received!');
+                
+                // get the latest message content
+                var contentPath = 'users/' + Auth.$getAuth().uid + '/decks/' + scope.activeDeckId + 
+                                    '/slides/' + scope.slideId + '/content';
+                
+                var content = $firebaseObject(Ref.child(contentPath));
+                content.$loaded().then(function(){
+                  $log.debug(content);
+                  scope.messages.push({body:content.message,timestamp:content.timestamp});  
+                });
               }
             });
           }
