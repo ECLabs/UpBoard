e4ac19ce86913ae9e587e69f982392f442a1620b
pauly815 <pauly815@hotmail.com>
2016-01-26 14:56:57 -0500

    UP-25 - removed Activity widget, added second uptime widget

--- app/frontend/scripts/directives/slides/ub-dashboard.js
+++ app/frontend/scripts/directives/slides/ub-dashboard.js
@@ -24,6 +24,7 @@
       controller: ['$scope', function($scope){
         
         // TODO initial data pull
+        $scope.docsIngestedPerSrcCategories = ['twitter', 'reddit'];
         $scope.docsIngestedPerSrcSeries = [{data:[420, 192]}]
         
         $scope.twitterIngestRateSeries = [{data:[103]}];
@@ -32,7 +33,8 @@
         $scope.redditIngestRateSeries = [{data:[51]}];
         $scope.redditIngestRateCurrent = 51;
         
-        $scope.uptimeHoursSeries = [{data: [97]}];
+        $scope.twitterUptimeHoursSeries = [{data: [91]}];
+        $scope.redditUptimeHoursSeries = [{data: [49]}];
         
         
         $scope.docsIngestedPerSrcConfig = {
@@ -45,7 +47,7 @@
               height: 100
             },
             xAxis: {
-              categories: ['twitter', 'reddit'],
+              categories: $scope.docsIngestedPerSrcCategories,
               title: {
                 text: null
               },
@@ -251,7 +253,7 @@
         }
         
         
-        $scope.uptimeHoursConfig = {
+        $scope.twitterUptimeHoursConfig = {
           
           options: {
             chart: {
@@ -318,11 +320,98 @@
               y: 15
             }  
           },
-          series: $scope.uptimeHoursSeries,
+          series: $scope.twitterUptimeHoursSeries,
+          loading: false
+        }
+        
+        $scope.redditUptimeHoursConfig = {
+          
+          options: {
+            chart: {
+              type: 'solidgauge',
+              backgroundColor: null,
+              borderWidth: 0,
+              height: 150
+            },
+            pane: {
+              center: ['50%', '85%'],
+              size: '140%',
+              startAngle: -90,
+              endAngle: 90,
+              background: {
+                backgroundColor:'#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+              }
+            },
+            exporting: {
+              enabled: false
+            },
+            tooltip: {
+              hideDelay: 0,
+              followPointer: true,
+              formatter: function(){
+                return this.y + '%';
+              }
+            },
+            plotOptions: {
+              solidgauge: {
+                dataLabels: {
+                  y: -30,
+                  borderWidth: 0,
+                  format: '<div style="text-align:center"><span style="font-size:25px;color:white;">{y}</span><br/>' +
+                       '<span style="font-size:12px;color:silver">%</span></div>',
+                  useHTML: true
+                }
+              }
+            },
+          },
+          title: {
+              text: null,
+          },
+          yAxis: {
+            min: 0,
+            max: 100,
+//            title: {
+//                y: 140
+//            },      
+            stops: [
+              [0.1, '#DF5353'], // red
+              [0.5, '#DDDF0D'], // yellow
+              [0.9, '#55BF3B'] // green
+            ],
+            lineWidth: 0,
+            tickInterval: 100,
+            //tickInterval: 20,
+//            tickPixelInterval: 400,
+            tickWidth: 0,
+            labels: {
+              style: {"color":"white"},
+              y: 15
+            }  
+          },
+          series: $scope.redditUptimeHoursSeries,
           loading: false
         }
       }],
       link: function postLink(scope, element, attrs) {
+        
+        function updateIngestCount(source, count){
+          
+          scope[source + 'IngestCountToday'] = count;
+          
+          var index = scope.docsIngestedPerSrcCategories.indexOf(source);
+          var data = scope.docsIngestedPerSrcSeries[0].data;
+          if(index === 0) {
+            data.shift();
+            data.unshift(count);
+          }
+          else {
+            data.pop();
+            data.push(count);
+          }
+        }
         
         function updateTotalIngest(){
           
@@ -331,8 +420,10 @@
           
           // add up counts from all sources, calculate delta
           for(var i = 0; i < scope.sources.length; i++){
-            scope.totalDocsIngestedToday += scope[scope.sources[i] + 'IngestCountToday'];
-            scope.totalDocsIngestedYesterday += scope[scope.sources[i] + 'IngestCountYesterday'];
+            
+            var source = scope.sources[i];
+            scope.totalDocsIngestedToday += scope[source + 'IngestCountToday'];
+            scope.totalDocsIngestedYesterday += scope[source + 'IngestCountYesterday'];
           }
           
           // calculate delta
@@ -358,6 +449,17 @@
           if(data.length > 20) data.shift();
         }
         
+        function updateUptime(source, totalMinutes, downMinutes){
+          
+          scope[source + 'UptimeHours'] = Math.round((totalMinutes - downMinutes) / 60);
+          scope[source + 'UptimePercentage'] = Math.round(((totalMinutes - downMinutes) / totalMinutes) * 100);
+          
+          // update gauge
+          var data = scope[source + 'UptimeHoursConfig'].series[0].data;
+          data.shift();
+          data.push(scope[source + 'UptimePercentage']);
+        }
+        
         
         // initial pull from scope.data, after that use socket event listeners to update via POSTS
         
@@ -370,16 +472,20 @@
           if(source === 'twitter'){
             scope[source + 'ErrorCountToday'] = 21;
             scope[source + 'ErrorCountYesterday'] = 101;
-            scope[source + 'IngestCountToday'] = 420;
             scope[source + 'IngestCountYesterday'] = 4500;
-            scope[source + 'IngestRateToday'] = 103;
+            scope[source + 'Status'] = 'running';
+            updateIngestCount(source, 420);
+            updateIngestRate(source, 103);
+            updateUptime(source, 3340, 300);
           }
           else if(source === 'reddit'){
             scope[source + 'ErrorCountToday'] = 7;
             scope[source + 'ErrorCountYesterday'] = 21;
-            scope[source + 'IngestCountToday'] = 192;
             scope[source + 'IngestCountYesterday'] = 1250;
-            scope[source + 'IngestRateToday'] = 51;
+            scope[source + 'Status'] = 'stopped';
+            updateIngestCount(source, 192);
+            updateIngestRate(source, 51);
+            updateUptime(source, 1000, 501);
           }
           updateErrorRateAndDelta(source);
         }
@@ -387,19 +493,24 @@
         updateTotalIngest();
         
         
-       /* Incoming JSON Data
-        source: 'twitter,
-        content: {
-          today:{
-            errorCount: 21,
-            ingestCount: 420,
-            ingestRate: 103
+       /* Incoming request body
+        event   = ingestEvent
+        source  = twitter
+        content = {
+          "today":{
+            "errorCount": 21,
+            "ingestCount": 420,
+            "ingestRate": 103
           }
-          yesterday:{
-            errorCount: 150
-            ingestCount: 4500
+          "yesterday":{
+            "errorCount": 150
+            "ingestCount": 4500
           },
-          status: 'running'
+          "status": "running",
+          "uptime":{
+            "totalMinutes": 3340,
+            "downMinutes": 300
+          }
         }
         */
         // make event configurable via scope data
@@ -411,7 +522,6 @@
           var source = data.source;
           var content = JSON.parse(data.content);
           
-          // TODO update to handle array of source data
           if(content != null){
 
             if(content.today != null){
@@ -424,7 +534,7 @@
               
               var ingestCountToday = content.today.ingestCount;
               if(ingestCountToday != null){
-                scope[source + 'IngestCountToday'] = ingestCountToday;
+                updateIngestCount(source, ingestCountToday);
                 updateTotalIngest();
               }
               
@@ -446,6 +556,19 @@
               if(ingestCountYesterday != null){
                 scope[source + 'IngestCountYesterday'] = ingestCountYesterday;
                 updateTotalIngest();
+              }
+            }
+            
+            if(content.status != null){
+              scope[source + 'Status'] = content.status;
+            }
+            
+            if(content.uptime != null){
+              
+              var totalMinutes = content.uptime.totalMinutes;
+              var downMinutes = content.uptime.downMinutes;
+              if(totalMinutes != null &&  downMinutes != null){
+                updateUptime(source, totalMinutes, downMinutes);
               }
             }
           }
@@ -477,15 +600,6 @@
         $interval(function () {
           updateSparklineChartDemo('reddit');
         }, 5000);
-        
-        // activity
-        
-        // uptime
-        
-        // status
-        
-        // docs/source
-        
       }
     };
   }
